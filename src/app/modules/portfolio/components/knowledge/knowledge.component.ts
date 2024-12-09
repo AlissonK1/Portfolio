e4ffe562eb18ed5a20eb656879e8e0ef6ea18a07
Alisson Kauan <Alisson.Kauan77@gmail.com>
2024-12-09 09:11:57 -0300

    Criação do Componente de Conhecimento

---
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,35 @@
+import { Component, Signal, signal } from '@angular/core';
+
+//Interfaces
+import { IKnowledge } from '../../interface/IKnowledge.interface';
+
+@Component({
+  selector: 'app-knowledge',
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: '/icons/knowledge/html5.svg',
+      alt: 'Ícone de conhecimento de html5'
+    },
+    {
+      src: '/icons/knowledge/css3.svg',
+      alt: 'Ícone de conhecimento de css3'
+    },
+    {
+      src: '/icons/knowledge/javascript.svg',
+      alt: 'Ícone de conhecimento de javascript'
+    },
+    {
+      src: '/icons/knowledge/angular.svg',
+      alt: 'Ícone de conhecimento de angular'
+    },
+    {
+      src: '/icons/knowledge/nodejs.svg',
+      alt: 'Ícone de conhecimento de nodejs'
+    },
+  ])
+}
